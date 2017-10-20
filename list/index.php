4e2d1e3695ee5ab86da3cdb62a3cda8eefaa97c8
Sam <Liney.Kin@yandex.ru>
2017-10-20 19:56:56 +0300

    new design of list

--- list/index.php
+++ list/index.php
@@ -23,26 +23,17 @@ $list = $project->getPlayersOrderByRating();
     <title>List</title>
     <link rel="stylesheet" href="../style/list.css">
 
-    <!--BOOTSTRAP-->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-    <!--/BOOTSTRAP-->
-
 </head>
 <body>
 
-<h2 id="h1_title" class="page-header">Рейтинг проекта "<?php echo $project->name; ?>"</h2>
-
-<table id="menu">
-    <tr>
-        <td><a href="../project/?code=<?php echo $projectCode?>">К выборам</a></td>
-        <td><a href="../../dvastula/">На главную</a></td>
-    </tr>
-</table>
+<div id="menu">
+    <table>
+        <tr>
+            <td><a href="../project/?code=<?php echo $projectCode?>">К выборам</a></td>
+            <td><a href="../../dvastula/">На главную</a></td>
+        </tr>
+    </table>
+</div>
 
 <div id="list" >
         <?php
@@ -75,15 +66,12 @@ $list = $project->getPlayersOrderByRating();
                 </tr>
                 <tr>
                     <td><span>Рейтинг <?php echo $player->rating?></span></td>
-                    <td><span>+ <?echo $player->wins?> - <?echo $player->fails?></span></td>
+                    <td><span><span class="plus">+ <?echo $player->wins?></span>  <span class="minus">- <?echo $player->fails?></span> </span></td>
                 </tr>
             </table>
 
         <?php } ?>
 </div>
-
-
-
 
 
 </body>
