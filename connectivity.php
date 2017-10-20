4182e805c6f89da0f7101526840f77835f120e64
Sam <Liney.Kin@yandex.ru>
2017-10-21 02:21:48 +0300

    refact2

--- connectivity.php
+++ connectivity.php
@@ -1,5 +1,19 @@
 <?php
 
-require_once "_connectivity/functions.php";
-require_once "_connectivity/classes.php";
-require_once "_connectivity/conf.php";
+$classesDir =  "_classes/";
+$dir =  "_connectivity/";
+
+require_once $dir."db_connect.php";
+require_once $dir."functions.php";
+require_once $dir."conf.php";
+
+$db_server = db_connect_server($db_hostname, $db_user, $db_name, $db_password);
+
+// ПОДКЛЮЧЕНИЕ КЛАССОВ
+require_once $classesDir."GameEngine.php";
+require_once $classesDir."Player.php";
+require_once $classesDir."Logger.php";
+require_once $classesDir."StringConverter.php";
+require_once $classesDir."Admin.php";
+require_once $classesDir."DB.php";
+require_once $classesDir."Project.php";
